f2e04977fc3ac1c18457eafa629c759732f07201
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2025-06-21 22:52:39 -0300

    se agrego la clase    conexion con los metodos  para conectar y cerrar la base  mas los paquetes necesarios

---
+++ src/main/java/Conexion/conexionDB.java
@@ -0,0 +1,74 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexionDB {
+
+    // Datos de conexión a tu base de datos MySQL
+    private static final String URL = "jdbc:mysql://localhost:3306/BancoTpIntegrador3";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "root"; 
+    
+    
+
+    public static Connection obtenerConexion() {
+        Connection conexion = null;
+        try {
+           
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            System.out.println("Conexión a la base de datos exitosa.");
+        } 
+        catch (ClassNotFoundException e) {
+            System.err.println("Error");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+
+    
+    public static void cerrarConexion(Connection conexion) {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                System.out.println("Conexión a la base de datos cerrada.");
+            } catch (SQLException e) {
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    
+    
+    
+    
+    //  ESTO ESTO ES PARA PROBAR LA CONEXION, LO PUEDEN BORRAR SI QUIEREN
+    public static void main(String[] args) {
+        Connection testConnection = obtenerConexion();
+        if (testConnection != null) {
+           
+            try {
+                System.out.println(" la conexion esta establecida " + testConnection.getCatalog());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                cerrarConexion(testConnection);
+            }
+        } else {
+            System.out.println(" Falló la conexión.");
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
