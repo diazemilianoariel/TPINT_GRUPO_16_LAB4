8a40a63e7e260f83b8e3194c887329e738fec8cb
LeonelGrosse <leonelgrosse@outlook.com>
2025-06-22 18:33:56 -0300

    Scripts BD modificados

--- scriptBD/TriggerClientes2106.sql
+++ scriptBD/TriggerClientes2106.sql
@@ -1,16 +1,17 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `bancotpintegrador3`.`clientes_BEFORE_INSERT` BEFORE INSERT ON `clientes` FOR EACH ROW
-BEGIN
+DELIMITER //
 
-DECLARE vIdUsuario INT;
+CREATE TRIGGER `BancoTpIntegrador3`.`Clientes_BEFORE_INSERT` 
+BEFORE INSERT ON `Cliente` FOR EACH ROW
+BEGIN
+  DECLARE vIdUsuario INT;
 
   IF NEW.IdUsuario IS NULL THEN
     INSERT INTO Usuario (NombreUsuario, Password, IdTipoUsuario, Estado)
     VALUES (NULL, NULL, 2, 1);
-
+    
     SET vIdUsuario = LAST_INSERT_ID();
     SET NEW.IdUsuario = vIdUsuario;
   END IF;
+END//
 
-
-
-END
+DELIMITER ;
--- scriptBD/procedimientoAlmacenadoCliente2106.sql
+++ scriptBD/procedimientoAlmacenadoCliente2106.sql
@@ -1,15 +1,17 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_IngresarCliente`(
+DELIMITER //
+
+CREATE PROCEDURE `sp_IngresarCliente`(
   IN pDni VARCHAR(10),
   IN pCuil VARCHAR(15),
   IN pNombre VARCHAR(50),
   IN pApellido VARCHAR(50),
   IN pSexo CHAR(1),
+  IN pNacionalidad VARCHAR(50),
   IN pFechaNacimiento DATE,
   IN pDireccion VARCHAR(100),
   IN pCorreoElectronico VARCHAR(50),
   IN pTelefono VARCHAR(50),
   IN pIdLocalidad INT
-  
 )
 BEGIN
   DECLARE vIdUsuario INT;
@@ -23,11 +25,13 @@ BEGIN
 
   -- 3. Insertar cliente con ese IdUsuario
   INSERT INTO Cliente (
-    Dni, Cuil, Nombre, Apellido, Sexo, FechaNacimiento,
+    Dni, Cuil, Nombre, Apellido, Sexo, Nacionalidad, FechaNacimiento,
     Direccion, CorreoElectronico, Telefono, IdLocalidad, IdUsuario, Estado
   )
   VALUES (
-    pDni, pCuil, pNombre, pApellido, pSexo, pFechaNacimiento,
-    pDireccion, pIdLocalidad, vIdUsuario, 1
+    pDni, pCuil, pNombre, pApellido, pSexo, pNacionalidad, pFechaNacimiento,
+    pDireccion, pCorreoElectronico, pTelefono, pIdLocalidad, vIdUsuario, 1
   );
-END
+END //
+
+DELIMITER ;
